d376fc51917224c8db5fe91bf1a6f6cf78131d0b
Sam Hadfield <sthtech@protonmail.com>
2025-07-22 09:18:39 -0500

    Add toggleVisibility fn

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,17 +1,14 @@
+import { toggleVisibility } from "./index.js";
+
 export const displayMenu = () => {
   const dropdownButton = document.querySelector("[data-button = 'dropdown']");
   const menuButtonContainer = document.querySelector(
-    "[data-container='menu-button']",
+    "[data-container = 'menu-buttons']",
   );
 
   dropdownButton.addEventListener("click", (event) => {
     event.preventDefault();
-    menuButtonContainer.show();
-  });
-
-  menuButtonContainer.addEventListener("blur", (event) => {
-    event.preventDefault();
-    menuButtonContainer.close();
+    toggleVisibility(menuButtonContainer);
   });
 };
 
